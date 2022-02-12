79b04d345e63110e49f011ebbc00485a1f1516f5
Adrian Nitu <adrian.nitu@gmail.com>
2022-02-12 07:04:51 +0200

    [fix] TypeScript defs

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,9 @@
-export interface NativeAudio {
+import type { Plugin } from '@capacitor/core';
+
+export interface NativeAudio extends Plugin {
   configure(options: ConfigureOptions): Promise<void>;
   preload(options: PreloadOptions): Promise<void>;
-  play(options: { assetId: string, time: number }): Promise<void>;
+  play(options: { assetId: string; time?: number }): Promise<void>;
   pause(options: { assetId: string }): Promise<void>;
   resume(options: { assetId: string }): Promise<void>;
   loop(options: { assetId: string }): Promise<void>;
